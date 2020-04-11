0e6c9c0d27cd00dfd48bc91597faf4c05aa3f518
LouMM <lmaresca@microsoft.com>
2020-04-11 14:29:51 -0700

    Optimizing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,63 +7,63 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 //const host = process.env.HOST || '0.0.0.0';
 
 module.exports = async (env, options) => {
-    const dev = options.mode === "development";
+    const devMode = options.mode === "development";
 
     const config = {
-        mode: "development",
-        optimization: {
-            minimize: false
-            /* minimizer: [
-                 new TerserPlugin({
-                     terserOptions: {
-                         parse: {
-                             // we want uglify-js to parse ecma 8 code. However, we don't want it
-                             // to apply any minfication steps that turns valid ecma 5 code
-                             // into invalid ecma 5 code. This is why the 'compress' and 'output'
-                             // sections only apply transformations that are ecma 5 safe
-                             // https://github.com/facebook/create-react-app/pull/4234
-                             ecma: 8
-                         },
-                         compress: {
-                             ecma: 5,
-                             warnings: false
-                             // Disabled because of an issue with Uglify breaking seemingly valid code:
-                             // https://github.com/facebook/create-react-app/issues/2376
-                             // Pending further investigation:
-                             // https://github.com/mishoo/UglifyJS2/issues/2011
-                             //,comparisons: false
-                         },
-                         mangle: {
-                             safari10: true
-                         },
-                         output: {
-                             ecma: 5,
-                             comments: false
-                             // Turned on because emoji and regex is not minified properly using default
-                             // https://github.com/facebook/create-react-app/issues/2488
-                             ,ascii_only: true
-                         }
-                     },
-                     // Use multi-process parallel running to improve the build speed
-                     // Default number of concurrent runs: os.cpus().length - 1
-                     parallel: true,
-                     // Enable file caching
-                     cache: true,
-                     sourceMap: true
-                 })
-             ],
-             // Automatically split vendor and commons
-             // https://twitter.com/wSokra/status/969633336732905474
-             // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-             splitChunks: {
-                 chunks: 'all',
-                 name: false,
-             },
-             // Keep the runtime chunk seperated to enable long term caching
-             // https://twitter.com/wSokra/status/969679223278505985
-             runtimeChunk: true,*/
-        },
-        devtool: "source-map",
+        mode: devMode,
+        optimization : {
+            minimize: devMode== "production" ? true : false
+            , minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        parse: {
+                            // we want uglify-js to parse ecma 8 code. However, we don't want it
+                            // to apply any minfication steps that turns valid ecma 5 code
+                            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                            // sections only apply transformations that are ecma 5 safe
+                            // https://github.com/facebook/create-react-app/pull/4234
+                            ecma: 8
+                        },
+                        compress: {
+                            ecma: 5,
+                            warnings: false
+                            // Disabled because of an issue with Uglify breaking seemingly valid code:
+                            // https://github.com/facebook/create-react-app/issues/2376
+                            // Pending further investigation:
+                            // https://github.com/mishoo/UglifyJS2/issues/2011
+                            //,comparisons: false
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        output: {
+                            ecma: 5,
+                            comments: false
+                            // Turned on because emoji and regex is not minified properly using default
+                            // https://github.com/facebook/create-react-app/issues/2488
+                            , ascii_only: true
+                        }
+                    },
+                    // Use multi-process parallel running to improve the build speed
+                    // Default number of concurrent runs: os.cpus().length - 1
+                    parallel: true,
+                    // Enable file caching
+                    cache: true,
+                    sourceMap: true
+                })
+            ],
+            // Automatically split vendor and commons
+            // https://twitter.com/wSokra/status/969633336732905474
+            // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+            splitChunks: {
+                chunks: 'all',
+                name: false,
+            },
+            // Keep the runtime chunk seperated to enable long term caching
+            // https://twitter.com/wSokra/status/969679223278505985
+            runtimeChunk: true,
+        }
+        ,devtool: "source-map",
         entry: {
             app: ['./src/index.tsx']
         }
